28d49bca0d91bc1c711fbb606fd787fbe6bfc1ef
Greenscarf <greenscarf005@gmail.com>
2020-04-20 08:13:38 +0900

    TCOD tutorials part #1

--- data.py
+++
@@ -1,5 +0,0 @@
-INVENTORY_ORDER = "abcdefghijklmnopqrstuvwxyz"
-
-POTION_APPEARANCE = ["fizzy", "orange", "thick", "charcoal"]
-POTION_CONTENT = ["acid", "regeneration", "poison", "fruit juice"] 
-
--- main.py
+++
@@ -1,9 +0,0 @@
-import tcod as libtcod
-
-
-def main():
-    print('Hello World!')
-
-
-if __name__ == '__main__':
-    main()
